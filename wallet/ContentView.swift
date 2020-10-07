85f99458bd9ce6a6dbc998e652657a8d9265bef0
Jacky <newbdez33@gmail.com>
2020-10-07 17:23:25 +0900

    Initial Commit

---
+++ wallet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  wallet
+//
+//  Created by Jacky on 2020/10/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ wallet/walletApp.swift
@@ -0,0 +1,17 @@
+//
+//  walletApp.swift
+//  wallet
+//
+//  Created by Jacky on 2020/10/07.
+//
+
+import SwiftUI
+
+@main
+struct walletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
